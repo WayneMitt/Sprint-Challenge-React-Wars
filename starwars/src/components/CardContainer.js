4f38f100ba2630de84fcdcd1f998bddc763412a6
Wayne Mitt <wmitt85@gmail.com>
2019-06-09 20:56:04 -0500

    project complete

--- starwars/src/components/CardContainer.js
+++ starwars/src/components/CardContainer.js
@@ -1,20 +1,30 @@
 import React from 'react';
 import CharName from './CharName'
+import './StarWars.css'
 
 const CardContainer = (props) => {
     console.log(props);
     return (
-      <div className='CardContainer'>
-            {props.charData.map(char => {
-                return (
-                    <CharName 
-                    name={char.name}
-                    />
-                )
-            })}
-          {/* <Appearance /> */}
-      </div>
+        <div className='CardContainer'>
+            {/* <div className='card'> */}
+                {props.charData.map(char => {
+                    return (
+                        <CharName
+                            name={char.name}
+                            height={char.height}
+                            mass={char.mass}
+                            hairColor={char.hair_color}
+                            eyeColor={char.eye_color}
+                            skinColor={char.skin_color}
+                            gender={char.gender}
+                            birthYear={char.birth_year}
+                        />
+                    )
+                })}
+            {/* </div> */}
+
+        </div>
     );
-  };
+};
 
 export default CardContainer;
